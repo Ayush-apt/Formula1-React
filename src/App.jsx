e983549f1ce9d91e2b66492d99647575231e0175
Ashu <ayushprajapati272007@gmail.com>
2026-08-31 21:43:41 +0530

    Driver Section(Working)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './components/Home/Home.jsx'
+import Driver from './components/Driver/Driver.jsx'
 
 const App = () => {
   return (
     <div >
       <Home />
+      <Driver />
       <div className="h-[2000px]">
-        <h1 className="pt-40 text-center text-4xl">
+        <h1 className="pt-70 text-center text-4xl">
           Scroll down
         </h1>
       </div>
